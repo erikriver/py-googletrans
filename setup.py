3bc23ce4b11790bc87fed1db9530b2604e407ced
Erik Rivera <erik@rivera.pro>
2024-06-08 14:33:22 -0600

    Updated httpx with http2

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ def install():
         packages=find_packages(exclude=['docs', 'tests']),
         keywords='google translate translator',
         install_requires=[
-            'httpx',
+            'httpx[http2]',
         ],
         python_requires= '>=3.6',
         tests_require=[
